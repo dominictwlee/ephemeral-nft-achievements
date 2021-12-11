107a3c70c80bf3422e53833d60f1854a0321641d
Dominic Lee <dominictwlee@gmail.com>
2021-12-11 21:02:34 +0000

    remove unused serializers

--- tests/util.ts
+++ tests/util.ts
@@ -1,37 +1,5 @@
 import ms from "ms";
-import * as BufferLayout from "buffer-layout";
-import * as borsh from "@project-serum/borsh";
 
 export function secs(format: string) {
   return Math.floor(ms(format) / 1000);
-}
-
-export function publicKeyLayout(property?: string) {
-  return BufferLayout.blob(32, property);
-}
-
-export function uint64Layout(property?: string) {
-  return BufferLayout.blob(8, property);
-}
-
-export function rustStringLayout(property = "string") {
-  return BufferLayout.struct(
-    [
-      BufferLayout.u32("length"),
-      BufferLayout.u32("lengthPadding"),
-      BufferLayout.blob(BufferLayout.offset(BufferLayout.u32(), -8), "chars"),
-    ],
-    property
-  );
-}
-
-export function vecLayout(property = "vec") {
-  return BufferLayout.struct(
-    [
-      BufferLayout.u32("length"),
-      BufferLayout.u32("lengthPadding"),
-      BufferLayout.blob(BufferLayout.offset(BufferLayout.u32(), -8), "chars"),
-    ],
-    property
-  );
 }
